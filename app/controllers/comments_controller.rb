d4b51d3efdfaf75ff655c056235be9c361dd2a11
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-02-19 12:18:06 +0000

    add comment delete function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,12 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
   private def comment_params
     params.require(:comment).permit(:username, :body)
   end
